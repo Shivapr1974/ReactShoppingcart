621b630f56faee4d34ae8dd9dcd0ada42a1d5a97
Siva Ram <Sivakumar.1.Ramakrishnan@gm.com>
2020-01-15 15:31:50 -0500

    Cart Logic

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,7 +1,8 @@
-import React, {  useState, useEffect } from 'react';
+import React, {  useRef, useState, useEffect } from 'react';
 import {Rtif} from '../Rtif';
 
 export default function ItemDetail({match}) {
+    const CART_KEY = 'cart-key';
     const imageStyle = {
         width: '500px',
 
@@ -9,13 +10,45 @@ export default function ItemDetail({match}) {
     const divStyle = {
         textAlign: 'center'
     }
+    const addToCartStyle = {
+        textAlign: 'center',
+        paddingLeft: '100px'
+    }    
+    const icon = {
+        width: '30px',
+        height: '30px',
+        paddingRight: '10px'
+    }
+    const listStyle = {
+        listStyleType: 'none'
+    }    
+    const navStyle = {
+        textDecoration: 'none'
+    }    
+
+
     const [item, setItem] = useState([ ]);
     const [items, setItems] = useState([ ]);
+    const [cart, setCart] = useState([ ]);
+    const cartRef = useRef();                    
 
     useEffect(()=>{
+        let cart = localStorage.getItem(CART_KEY);
+        console.log( 'Get: ' + JSON.parse(cart));
+        if(cart === null || cart === undefined){
+            cart = []
+            setCart(cart);            
+        }else{
+            setCart(JSON.parse(cart));           
+        }
         fetchItems();
         fetchItem();
     },[]) ; //Since array is empty it will be called once. 
+    useEffect(()=>{
+        localStorage.setItem(CART_KEY, JSON.stringify(cart));
+      },[cart]); //It will be called everytime todos [] change.
+      
+
     useEffect(()=>{
         console.log( item );
     },[item]); //It will be called everytime todos [] change.
@@ -29,6 +62,13 @@ export default function ItemDetail({match}) {
         // console.log( items );
     },[items]); //It will be called everytime todos [] change.
 
+    function handleCart(){
+        setCart(prevCart => {
+          return [...prevCart, {id:  match.params.id, name: item.name, icon: item.images.icon} ]
+        });
+    
+    }        
+
     const fetchItem = async () =>{
         const data = await fetch(`https://fortnite-api.theapinetwork.com/item/get?${match.params.id}`);
         const itemsJson = await data.json()
@@ -41,21 +81,41 @@ export default function ItemDetail({match}) {
         // console.log( itemsJson.data);
     }
     return (
-        <>
-            <div style={divStyle}>
-                <h1>{item.name} ({item.ratings?.avgStars} ratings)</h1> 
-                {/* <img src={item.images?.icon}></img>                  */}
-                <Rtif boolean={item.images?.featured !== null}>
-                    <img style={imageStyle}  src={item.images?.featured}></img>                 
-                </Rtif>                                  
+        <>  
+            <table >
+                <tr>
+                    <td>
+                        <div style={divStyle}>
+                            <h1>{item.name} ({item.ratings?.avgStars} ratings)</h1> 
+                            {/* <img src={item.images?.icon}></img>                  */}
+                            <Rtif boolean={item.images?.featured !== null}>
+                                <img style={imageStyle}  src={item.images?.featured}></img>                 
+                            </Rtif>                                  
 
-                <Rtif boolean={item.images?.featured === null}>
-                    <img style={imageStyle}  src={item.images?.icon}></img>                 
-                </Rtif>                                  
-                {/* <img src={item.images?.background}></img>                  */}
-                {/* <img src={item.images?.information}></img>                  */}
-                <h2><i>{item.description}</i></h2>
-            </div>
+                            <Rtif boolean={item.images?.featured === null}>
+                                <img style={imageStyle}  src={item.images?.icon}></img>                 
+                            </Rtif>                                  
+                            {/* <img src={item.images?.background}></img>                  */}
+                            {/* <img src={item.images?.information}></img>                  */}
+                            <h2><i>{item.description}</i></h2>
+                        </div>
+                    </td>
+                    <td>
+                        <div style={addToCartStyle}> 
+                            <button onClick={handleCart}>Add To Cart</button>
+                            <ul>
+                                {cart.map( data => (
+                                    <li  style={listStyle}  key={data.id}>
+                                        <img style={icon} src={data.icon}></img>                             
+                                        {data.name}
+                                    </li>
+                                ))}
+
+                            </ul>
+                        </div>
+                    </td>
+                </tr>
+            </table>
         </>
     )
 }
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -2,6 +2,7 @@ import React, {  useState, useEffect } from 'react';
 import {Link} from 'react-router-dom';
 
 export default function Shop() {
+    const CART_KEY = 'cart-key';
     const icon = {
         width: '30px',
         height: '30px',
@@ -13,10 +14,23 @@ export default function Shop() {
     const navStyle = {
         textDecoration: 'none'
     }    
+    const addToCartStyle = {
+        textAlign: 'center',
+        paddingLeft: '100px'
+    }        
     const [items, setItems] = useState([ ]);
+    const [cart, setCart] = useState([ ]);
 
     useEffect(()=>{
         fetchItems();
+        let cart = localStorage.getItem(CART_KEY);
+        console.log( 'Get: ' + JSON.parse(cart));
+        if(cart === null || cart === undefined){
+            cart = []
+            setCart(cart);            
+        }else{
+            setCart(JSON.parse(cart));           
+        }        
     },[]) ; //Since array is empty it will be called once. 
 
     useEffect(()=>{
@@ -33,20 +47,40 @@ export default function Shop() {
     }
     return (
         <>
-            <div>
-                <h1>Shop</h1>
-                <ul>
-                    {items.map( data => (
-                        <li style={listStyle} key={data.itemId}>
-                            <Link style={navStyle} to={`/shop/${data.itemId}/${data.item.name}`}> 
-                                <img style={icon} src={data.item.images.icon}></img> 
-                                {data.item.name} ({data.item.ratings.avgStars} ratings)
-                            </Link>
-                        </li>
-                    ))}
+            <table >
+                <tr>
+                    <td>
+                        <div>
+                            <h1>Shop</h1>
+                            <ul>
+                                {items.map( data => (
+                                    <li style={listStyle} key={data.itemId}>
+                                        <Link style={navStyle} to={`/shop/${data.itemId}/${data.item.name}`}> 
+                                            <img style={icon} src={data.item.images.icon}></img> 
+                                            {data.item.name} ({data.item.ratings.avgStars} ratings)
+                                        </Link>
+                                    </li>
+                                ))}
 
-                </ul>
-            </div>
+                            </ul>
+                        </div>
+                    </td>
+                    <td>
+                        <div style={addToCartStyle}> 
+                            <h1>Cart</h1>        
+                            <ul>
+                                {cart.map( data => (
+                                    <li  style={listStyle}  key={data.id}>
+                                        <img style={icon} src={data.icon}></img>                             
+                                        {data.name}
+                                    </li>
+                                ))}
+
+                            </ul>
+                        </div>
+                    </td>
+                </tr>
+            </table>
         </>
     )
 }
