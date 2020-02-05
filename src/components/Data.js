cbcafde2d3120e9e3ddbd0c08345a7ddf5407559
Siva Ram <Sivakumar.1.Ramakrishnan@gm.com>
2020-02-05 10:31:38 -0500

    Fix caching

--- src/components/Data.js
+++ src/components/Data.js
@@ -3,17 +3,20 @@ import { getContrastRatio } from '@material-ui/core';
 
 const fetchItemsData = async () =>{
     const CART_KEY = 'cart-key';
-    const data = await fetch('https://fortnite-api.theapinetwork.com/store/get');
-    const itemsJson = await data.json()
-    let availableItems = [];
-    if( itemsJson === null || itemsJson === undefined ||  
-        itemsJson.data === null || itemsJson.data === undefined || itemsJson.data.length === 0 ){
-            availableItems =  triviaJson;    
-    }else{
-        availableItems =  itemsJson.data;
+    try{
+        const data = await fetch('https://fortnite-api.theapinetwork.com/store/get');
+        const itemsJson = await data.json()
+        let availableItems = [];
+        if( itemsJson === null || itemsJson === undefined ||  
+            itemsJson.data === null || itemsJson.data === undefined || itemsJson.data.length === 0 ){
+                availableItems =  triviaJson;    
+        }else{
+            availableItems =  itemsJson.data;
+        }
+        return availableItems;
+    }catch(e){
+        return triviaJson;
     }
-    
-    return availableItems;
 }
 export default fetchItemsData;
 
